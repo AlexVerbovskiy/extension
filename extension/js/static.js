b94aee5b551be8f128bd109350adc48239310753
Verbovskiy Alexandr <cofeek5@gmail.com>
2022-12-27 16:15:59 +0200

    version 2

--- extension/js/static.js
+++ extension/js/static.js
@@ -4,8 +4,8 @@ const updateCacheTime = 60000; //time updating cached users
 
 const defaultUserImg = chrome.runtime.getURL("ghost.svg");
 
-//const serverURL = "http://ec2-34-204-76-116.compute-1.amazonaws.com/";
-const serverURL = "http://127.0.0.1:8000/";
+const serverURL = "http://ec2-34-204-76-116.compute-1.amazonaws.com/";
+//const serverURL = "http://127.0.0.1:8000/";
 const apiURL = serverURL + "api/";
 
 const iconSrc = chrome.runtime.getURL("heart.svg");
